4f707e1d369793efc5184dbd9623f7e215a1a906
ElfraimBloodwell <40169533+ElfraimBloodwell@users.noreply.github.com>
2021-06-30 23:27:41 -0300

    Add files via upload

---
+++ TPINT_GRUPO_3_LAB4v1/src/main/java/servlets/ServletCuentas.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dominio.Cuenta;
+import dominio.CuentasDao;
+
+/**
+ * Servlet implementation class ServletCuentas
+ */
+@WebServlet("/ServletCuentas")
+public class ServletCuentas extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public ServletCuentas() {
+        super();
+
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		if(request.getParameter("Listar")!=null) {
+			
+			CuentasDao cdao = new CuentasDao();
+			ArrayList<Cuenta> lista = cdao.obtenerCuentas();
+			
+			request.setAttribute("ListaC", lista);
+			RequestDispatcher rd = request.getRequestDispatcher("/ABMLCuentas_Listado.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+
+}
